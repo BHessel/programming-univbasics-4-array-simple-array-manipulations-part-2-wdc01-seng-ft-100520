01614cab4879063ffbcabca073d8c90a1428bb4c
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-13 02:03:27 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(my_favorite_things, more_favs)
+  my_favorite_things.concat more_favs
+end
+
+def using_insert (list_of_programming_languages, another_language)
+  list_of_programming_languages.insert(4,another_language)
+end
+
+def using_uniq (haircuts)
+  haircuts.uniq
+end
+
+def using_flatten (flat_array)
+  flat_array.flatten  
+end
+
+def using_delete (no_offense_steven, instructors)
+  no_offense_steven.delete ("Steven")
+end
+
+def using_delete_at (deleted_robot, famous_robots)
+  deleted_robot.delete_at(2)
+end
